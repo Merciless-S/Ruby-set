3168ed6f9261e60a8f9e36dcbdd87ab146746032
Fengze Wu <roarkwfz@gmail.com>
2020-09-20 13:46:12 -0400

    cards images

---
+++ app/views/shoes/index.rb
@@ -0,0 +1,9 @@
+require 'shoes'
+Shoes.app {
+    stack(margin: 6) {
+      title "Welcome to the game of set!"
+      para strong("Q."), " Are you beginning to grasp hold of Shoes?"
+      para em(strong("A."), " Quit pestering me, I'm hacking here.")
+    }
+  }
+  
